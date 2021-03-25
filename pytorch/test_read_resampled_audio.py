b92fed313d11df28e196bd1a7945852e5535d638
FaithKaixuan <1102093868@qq.com>
2021-03-25 12:52:57 +0100

    Update test_read_resampled_audio.py

--- pytorch/test_read_resampled_audio.py
+++ pytorch/test_read_resampled_audio.py
@@ -3,12 +3,16 @@ import librosa
 import pandas as pd
 
 train_csv = pd.read_csv("Germany_Birdcall_resampled_filtered.csv")
+datadir = "/mnt/GermanyBirdcall/Germany_Birdcall_resampled"
+
 idx = 19783
 sample = train_csv.loc[idx, :]
 wav_name = sample["filename"]
 gen = sample["gen"]
 sp = sample["sp"]
 # file_path = self.datadir / ebird_code / mp3_name
-file_path = self.datadir + "/" + gen + "/"+ sp + "/" + wav_name
+file_path = datadir + "/" + gen + "/"+ sp + "/" + wav_name
+
 # y = librosa.core.load(file_path, SR)[0]
 y, sr = sf.read(file_path)
+print(y)
