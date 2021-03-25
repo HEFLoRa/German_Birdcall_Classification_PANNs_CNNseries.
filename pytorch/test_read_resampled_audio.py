61c12af2ae34c4e3033ad27f7025690e7b0a90ac
FaithKaixuan <1102093868@qq.com>
2021-03-25 13:43:25 +0100

    Update test_read_resampled_audio.py
    Try copied filepath from error

--- pytorch/test_read_resampled_audio.py
+++ pytorch/test_read_resampled_audio.py
@@ -12,7 +12,7 @@ gen = sample["gen"]
 sp = sample["sp"]
 # file_path = self.datadir / ebird_code / mp3_name
 file_path = datadir + "/" + gen + "/"+ sp + "/" + wav_name
-
+file_path = '/mnt/Germany_Birdcall/Germany_Birdcall_resampled/Merops/apiaster/XC500595-190914 Bienenfresser Zugruf FH 1701.wav'
 # y = librosa.core.load(file_path, SR)[0]
 y, sr = sf.read(file_path)
 print(y)
